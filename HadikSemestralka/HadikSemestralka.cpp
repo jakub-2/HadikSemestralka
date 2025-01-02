860f1534f75e639c733f41642efa5cb589356a35
jakub-2 <jakubjanco@gmail.com>
2025-01-02 16:41:55 +0100

    Add project files.

---
+++ HadikSemestralka/HadikSemestralka.cpp
@@ -0,0 +1,12 @@
+﻿// HadikSemestralka.cpp : Defines the entry point for the application.
+//
+
+#include "HadikSemestralka.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
