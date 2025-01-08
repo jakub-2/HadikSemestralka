cbded96fd0720882511c14649c345bae0118006f
jakub-2 <jakubjanco@gmail.com>
2025-01-08 14:43:34 +0100

    Client a upraveny ClientBasic

--- HadikSemestralka/HadikSemestralka.c
+++ HadikSemestralka/HadikSemestralka.c
@@ -1,5 +1,4 @@
-﻿#include "Hra.h"
-#include "Menu.h"
+﻿#include "Menu.h"
 
 #define DELAY_SIRKA 100000
 #define DELAY_VYSKA 150000
@@ -11,14 +10,6 @@ int main() {
     char* poleSvetov[] = { "Svet bez prekazok", "Svet s prekazkami"};
 
     int vybranySvet = menu(poleSvetov, velkost);
-
-    if (vybranySvet == 0)
-    {
-        hra(0);
-    } else if (vybranySvet == 1)
-    {
-        hra(1);
-    }
 
     return 0;
 }
